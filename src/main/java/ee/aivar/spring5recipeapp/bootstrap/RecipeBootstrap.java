cb552a409d419c270b536c494cd8ce750bd037f5
Aivar Mägi <aivar.magi@cgi.com>
2017-11-06 15:12:09 +0200

    display remaining recipe properties

--- src/main/java/ee/aivar/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/ee/aivar/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -117,6 +117,8 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving.\n");
 
         guacamoleRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacamoleRecipe.setServings(4);
+        guacamoleRecipe.setSource("Simpy recipes");
 
         Notes guacamoleNotes = new Notes();
         guacamoleNotes.setRecipeNotes("For a very quick guacamole just take a 1/4 cup of salsa and mix it in with your mashed avocados.\n" +
@@ -124,7 +126,6 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "The simplest version of guacamole is just mashed avocados with salt. Don't let the lack of availability of other ingredients stop you from making guacamole.\n" +
                 "To extend a limited supply of avocados, add either sour cream or cottage cheese to your guacamole dip. Purists may be horrified, but so what? It tastes great.");
         guacamoleRecipe.setNotes(guacamoleNotes);
-        guacamoleNotes.setRecipe(guacamoleRecipe);
 
         guacamoleRecipe.addIngredient(new Ingredient("ripe avocados", new BigDecimal(2), eachUom));
         guacamoleRecipe.addIngredient(new Ingredient("Kosher salt", new BigDecimal(0.5), teaSpoonUom));
@@ -156,10 +157,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n" +
                 "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n" +
                 "\n" +
-                "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.\n" +
-                "\n" +
-                "\n" +
-                "Read more: http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+                "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.\n");
+
+        tacoRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacoRecipe.setServings(4);
+        tacoRecipe.setSource("Simpy recipes");
 
         Notes tacoNotes = new Notes();
         tacoNotes.setRecipeNotes("Look for ancho chile powder with the Mexican ingredients at your grocery store, on buy it online. (If you can't find ancho chili powder, you replace the ancho chili, the oregano, and the cumin with 2 1/2 tablespoons regular chili powder, though the flavor won't be quite the same.)");
