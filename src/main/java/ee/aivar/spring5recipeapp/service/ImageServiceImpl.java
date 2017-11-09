396b52b81ddc8af547a69e152c1452ef32d5877a
Aivar Mägi <aivar.magi@cgi.com>
2017-11-09 08:02:31 +0200

    persist image to database

--- src/main/java/ee/aivar/spring5recipeapp/service/ImageServiceImpl.java
+++ src/main/java/ee/aivar/spring5recipeapp/service/ImageServiceImpl.java
@@ -1,9 +1,12 @@
 package ee.aivar.spring5recipeapp.service;
 
+import ee.aivar.spring5recipeapp.domain.Recipe;
 import ee.aivar.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -17,6 +20,25 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-//        recipeRepository.
+        log.debug("Saving file of recipe with id: {}", recipeId);
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] bytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                bytes[i++] = b;
+            }
+
+            recipe.setImage(bytes);
+
+            recipeRepository.save(recipe);
+        } catch (IOException ex) {
+            log.error("Error during image save", ex);
+
+            ex.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/ee/aivar/spring5recipeapp/service/ImageServiceImplTest.java
@@ -0,0 +1,56 @@
+package ee.aivar.spring5recipeapp.service;
+
+import ee.aivar.spring5recipeapp.domain.Recipe;
+import ee.aivar.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ImageServiceImplTest {
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    private ImageService imageService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+
+        Long id = 1L;
+        MultipartFile file = new MockMultipartFile("imagefile", "testing.txt", "text/plain", "testfile".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        imageService.saveImageFile(id, file);
+
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(file.getBytes().length, savedRecipe.getImage().length);
+    }
+}
